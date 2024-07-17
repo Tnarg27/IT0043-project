8e980e08df1c4e3d751d138127bfbe27c578f7d8
Tnarg27 <167262903+Tnarg27@users.noreply.github.com>
2024-07-17 18:12:48 +0800

    Final

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Final-Project
-IT0043 - Project
+# IT0043L (Web Design with Client-Side Scripting) - Final Project
+
+Created by: Grant Paul Arnel P. Pamor
+
+Section: TX02
---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+let label = document.querySelectorAll("label").forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letters, i) =>
+        `<span style="transition-delay: ${i * 50}ms"> ${letters}</span>`
+    )
+    .join("");
+});
